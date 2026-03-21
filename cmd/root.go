1544388d12dfb0a4e0e0aec0a42d35d0c4d0ad26
Max Lund <lundmax@gmail.com>
2026-03-20 21:55:49 -0500

    Clean up plugins on exit via CloseAll and signal handler

--- cmd/root.go
+++ cmd/root.go
@@ -3,8 +3,12 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/spf13/cobra"
+
+	"squadron/plugin"
 )
 
 var rootCmd = &cobra.Command{
@@ -16,6 +20,18 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
+	// Ensure all plugins are cleaned up on exit
+	defer plugin.CloseAll()
+
+	// Also clean up plugins on signals (SIGKILL can't be caught, but SIGINT/SIGTERM can)
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-sigCh
+		plugin.CloseAll()
+		os.Exit(1)
+	}()
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
